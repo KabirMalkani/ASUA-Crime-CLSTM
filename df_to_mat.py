ad87b2d114b2e2f373b2b87c312267f5d886521d
Kabir Malkani <kabir.malkani97@gmail.com>
2020-03-24 02:32:34 -0400

    all inputs added

--- df_to_mat.py
+++ df_to_mat.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-import more_itertools as mit
+# import more_itertools as mit
 from itertools import groupby
 from tqdm import tqdm
-from collections import OrderedDict
+# from collections import OrderedDict
 
 df = pd.read_csv("crimedata-final.csv")
 
@@ -14,32 +14,50 @@ sub = 1388534400000000000
 df['time'] = ((df['time'].astype(int)-sub)/div).astype(int)
 df = df.sort_values('time')
 
-drop_columns = ['occurrencehour', 'occurrencedate', 'Hood_ID', 'ObjectId', 'Division', 'event_unique_id', 'Neighbourhood_x', 'offence', 'premisetype', 'wind_dir', 'unixtime']
-df = df.drop(columns=drop_columns)
-df = pd.get_dummies(df)
+# drop_columns = ['occurrencehour', 'occurrencedate', 'Hood_ID', 'ObjectId', 'Division', 'event_unique_id', 'Neighbourhood_x', 'offence', 'premisetype', 'wind_dir', 'unixtime']
+# df = df.drop(columns=drop_columns)
+# df = pd.get_dummies(df)
 
 time_group = [list(v) for l,v in groupby(df.index, lambda x: df['time'][x])]
 
-df = df.drop(columns='time')
+df = df[df.columns[[13, 14, 16, 17, 19, 20, 22, 23, 24, 27, 28, 29]]]
 
-xdim = 32
-ydim = 32
-# adim = len(df.columns[3::])
+# df = df.drop(columns='time')
+
+# xdim = 128
+# ydim = 128
+adim = len(df.columns)
+# # adim = len(df.columns[2::])
+# # adim = len(df.columns[3::])
 tdim = len(time_group)
 
-df[['Lat']] = (MinMaxScaler().fit_transform(df[['Lat']])*(xdim-1)).round()
-df[['Long']] = (MinMaxScaler().fit_transform(df[['Long']])*(ydim-1)).round()
+# df[['Lat']] = (MinMaxScaler().fit_transform(df[['Lat']])*(xdim-1)).round()
+# df[['Long']] = (MinMaxScaler().fit_transform(df[['Long']])*(ydim-1)).round()
 
-# matrix = np.zeros((tdim, xdim, ydim, adim))
-matrix = np.zeros((tdim, xdim, ydim))
+# # # matrix = np.zeros((tdim, xdim, ydim, adim))
+# # matrix = np.zeros((tdim, xdim, ydim))
+# matrix = np.zeros((xdim, ydim, adim))
+matrix = np.zeros((tdim, adim))
 
 for i, t in tqdm(enumerate(time_group)):
 	time_df = df.iloc[t,:].drop_duplicates()
 
+# c = 0
+# for _, row in tqdm(df.iterrows()):
 	for x, row in time_df.iterrows():
-		# matrix[i, int(row["Lat"]), int(row["Long"]), :] = np.array(row[3:])
-		matrix[i, int(row["Lat"]), int(row["Long"])] += 1
+		matrix[i, :] = np.array(row)
+# 	# if (matrix[int(row["Lat"]), int(row["Long"]), :]  == np.zeros((adim))).all():
+# 	matrix[int(row["Lat"]), int(row["Long"]), :] = np.array(row[2:])
+# 	# else:
+# 		# c += 1
+# 		# print(c)
 
+# 	# if c == 32*32:
+# 		# break
+# 		# matrix[i, int(row["Lat"]), int(row["Long"]), :] = np.array(row[3:])
+# # 		matrix[i, int(row["Lat"]), int(row["Long"])] += 1
 
-np.save('first_layer', matrix)
+print(matrix[24, :])
+
+np.save('weather', matrix)
 print("done!")
--- mixed_input.py
+++ mixed_input.py
@@ -1,71 +1,119 @@
-from keras.layers import Input, Dense, concatenate, LSTM, ConvLSTM2D, BatchNormalization, Conv2D
+from keras.layers import Input, Dense, add, concatenate, multiply, LSTM, ConvLSTM2D, BatchNormalization, Conv2D, AveragePooling2D, Reshape, Lambda
 from keras.models import Model
+from keras import backend as K
 import time
 import numpy as np
+from keras.utils import plot_model
+
 tm = time.clock()
 
+# Load environment data
+og_env = np.load('environment.npy')
+og_env = np.reshape(og_env, (1, 128, 128, 19))
 
-og_mat = np.load('first_layer.npy')
+# Load weather data
+og_wet = np.load('weather.npy')
 
-tdim, xdim, ydim = og_mat.shape
+# # Load and fix crime data
+og_crime = np.load('first_layer.npy')
+
+tdim, xdim, ydim = og_crime.shape
 
 n_sample = 320
 batch_size = 36
 mat = np.zeros((n_sample, batch_size, xdim, ydim, 1))
+wet = np.zeros((n_sample, batch_size, og_wet.shape[1]))
 
 for i in range(n_sample):
 	a = np.random.randint(tdim - batch_size)
-	mat[i, 0:batch_size, :, :, 0] = og_mat[a:a+batch_size, :, :]
+	mat[i, 0:batch_size, :, :, 0] = og_crime[a:a+batch_size, :, :]
+	wet[i, 0:batch_size, :] = og_wet[a:a+batch_size, :]
 
 x = mat[:, :-1, :, :, :]
+wet = wet[:, :-1, :]
 y = np.reshape(mat[:, -1, :, :, :], (n_sample, xdim, ydim, 1))
 
-print(x.shape, y.shape)
-
-print("Preprocess: ", time.clock() - tm)
-tm = time.clock()
 
 
+# # Mixed Input Model
+
+# Environment
+input_env = K.variable(og_env)
+input_env = Input(tensor=input_env)
+
+env = AveragePooling2D(pool_size=(2, 2), data_format='channels_last')(input_env)
+
+env = Conv2D(filters=40, 
+	kernel_size=(2, 2),
+	strides=(2, 2),
+	activation='sigmoid',
+	padding='valid',
+	data_format='channels_last')(env)
+
+env = BatchNormalization()(env)
+
+
+# Weather
+input_wet = Input(shape = (batch_size-1, 12))
+
+weather = LSTM(
+	units=20,
+	return_sequences=False)(input_wet)
+
+weather = BatchNormalization()(weather)
+
+
+# Crime Layers
 input_crime = Input(shape = (batch_size-1, xdim, ydim, 1))
 
 crime = ConvLSTM2D(
-	filters=1, 
-	kernel_size=(3, 3), 
+	filters=20, 
+	kernel_size=(5, 5), 
 	padding='same', 
 	return_sequences=True)(input_crime)
 
 crime = BatchNormalization()(crime)
 
+crime = multiply([crime, weather])
+
 crime = ConvLSTM2D(
-	filters=1, 
-	kernel_size=(3, 3), 
+	filters=40, 
+	kernel_size=(4, 4), 
 	padding='same', 
 	return_sequences=True)(crime)
 
 crime = BatchNormalization()(crime)
 
+crime = add([env, crime])
+
 crime = ConvLSTM2D(
-	filters=1, 
-	kernel_size=(3, 3), 
+	filters=50, 
+	kernel_size=(2, 2), 
 	padding='same', 
 	return_sequences=False)(crime)
 
-# crime = BatchNormalization()(crime)
+crime = BatchNormalization()(crime)
 
-# crime = Conv2D(filters=1, 
-# 	kernel_size=(3, 3),
-# 	activation='sigmoid',
-# 	padding='same',
-# 	data_format='channels_last')(crime)
-
-model = Model(inputs=input_crime, outputs=crime)
-print(model.summary())
-model.compile(optimizer='adadelta', loss='binary_crossentropy')
-model.fit(x, y, epochs=20, batch_size=32)
-
-print("Train: ", time.clock() - tm)
-tm = time.clock()
+crime = Conv2D(
+	filters=1, 
+	kernel_size=(1, 1), 
+	padding='same')(crime)
 
 
-ypred = model.predict(x[0:1])
-print(ypred.shape)
+# print(env.shape)
+# print(crime.shape)
+
+
+model = Model(inputs=[input_wet, input_env, input_crime], outputs=crime)
+plot_model(model, to_file='model.png', show_shapes=True)
+
+
+# model.compile(optimizer='adadelta', loss='binary_crossentropy')
+# model.fit(x, y, epochs=20, batch_size=32)
+
+# print("Train: ", time.clock() - tm)
+# tm = time.clock()
+
+
+# ypred = model.predict(x[0:1])
+# print(ypred.shape)
